a555e990ff05ac105221caccf40555b2ffaa2f41
vikash sajwal <vikassajwal@gmail.com>
2026-01-01 13:20:07 +0530

    Add grayscale image example

---
+++ grayscale.py
@@ -0,0 +1,11 @@
+import cv2
+
+img = cv2.imread("test.jpg")
+
+if img is None:
+    print("Image not found")
+else:
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Grayscale Image", gray)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
